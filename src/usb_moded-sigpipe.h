6d19366c5c9fff70c0337e1288b33c6834e3374c
Bart Ribbers <bribbers@disroot.org>
2020-03-25 12:48:55 +0100

    Define TEMP_FAILURE_RETRY on non-glibc systems
    This makes it work for example on Musl systems

--- src/usb_moded-sigpipe.h
+++ src/usb_moded-sigpipe.h
@@ -37,4 +37,14 @@
 
 bool sigpipe_init(void);
 
+/* Used to retry syscalls that can return EINTR. Taken from Bionic unistd.h */
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(exp) ({         \
+    __typeof__(exp) _rc;                   \
+    do {                                   \
+        _rc = (exp);                       \
+    } while (_rc == -1 && errno == EINTR); \
+    _rc; })
+#endif
+
 #endif /* USB_MODED_SIGPIPE_H_ */
